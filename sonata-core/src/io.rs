e23f2e2111c1a18af2111749e3354c826820f9c0
Philip Deljanov <philip.deljanov@gmail.com>
2019-03-05 15:44:26 -0500

    Add rewind() support to the MediaSourceStream.
    Rewinding allows a stream to be backtracked without touching the underlying source.
    Unseekable sources are therefore given a modest capability for backwards seeking.
    This functionality is provided in lieu of "peek" functions, and is more powerful.
    
    MediaSourceStream also gains an exponentially growing read-ahead buffer to reduce
    the overhead of seeks.

--- sonata-core/src/io.rs
+++ sonata-core/src/io.rs
@@ -131,7 +131,13 @@ impl<T: std::convert::AsRef<[u8]>> MediaSource for io::Cursor<T> {
 
 /// A `MediaSourceStream` is the common reader type for Sonata. `MediaSourceStream` uses type erasure to mask the 
 /// inner reader from the consumer. Allowing any typical source to be used. Dynamic dispatch overhead is reduced by 
-/// buffering the inner reader.
+/// buffering the inner reader. 
+///
+/// The amount of buffered bytes is 8kB, however an additional 8kB is used for a rewind buffer. The rewind buffer allows
+/// non-seekable sources to be seeked backwards. A source is therefore guaranteed to be backwards seekable by 8kB so 
+/// long as the current read position is >8kB (can't seek to a negative position) and underlying source has a length 
+/// >8kB. Additionally, the read-ahead buffer provides cheap forward seeks if the new position remains within the 
+/// read-ahead buffer.
 pub struct MediaSourceStream {
     /// The source reader.
     inner: Box<dyn MediaSource>,
@@ -143,26 +149,181 @@ pub struct MediaSourceStream {
     pos: usize,
 
     /// The total number of valid bytes in buf.
-    byte_length: usize,
+    end_pos: usize,
+
+    cur_capacity: usize,
+
+    part_idx: u32,
+    part: [Partition; 2],
+}
+
+struct Partition {
+    base_pos: u64,
+    len: usize,
+    capacity: usize,
 }
 
 impl MediaSourceStream {
 
+    /// The maximum capacity of the read-ahead buffer. Must be a power-of-2.
+    const MAX_CAPACITY:  usize = 32 * 1024;
+
+    /// The initial capacity of the read-ahead buffer. Must be less than MAX_CAPACITY, and a power-of-2.
+    const INIT_CAPACITY: usize =  1 * 1024;
+
     pub fn new(source: Box<dyn MediaSource>) -> Self {
         MediaSourceStream {
             inner: source,
-            buf: vec![0u8; 32 * 1024].into_boxed_slice(),
+            cur_capacity: Self::INIT_CAPACITY,
+            buf: vec![0u8; 2 * Self::MAX_CAPACITY].into_boxed_slice(),
             pos: 0, 
-            byte_length: 0,
+            end_pos: 0,
+            part_idx: 0,
+            part: [
+                Partition { base_pos: 0, len: 0, capacity: Self::INIT_CAPACITY },
+                Partition { base_pos: 0, len: 0, capacity: Self::INIT_CAPACITY },
+            ],
         }
     }
 
-    fn fetch_buffer(&mut self) -> io::Result<&[u8]> {
-        if self.pos >= self.byte_length {
-            self.byte_length = self.inner.read(&mut self.buf)?;
-            self.pos = 0;
+    /// Invalidate the read-ahead buffer.
+    fn invalidate(&mut self, base_pos: u64) {
+        self.pos = 0;
+        self.end_pos = 0;
+        self.cur_capacity = Self::INIT_CAPACITY;
+        self.part_idx = 0;
+        self.part = [
+            Partition { base_pos, len: 0, capacity: Self::INIT_CAPACITY },
+            Partition { base_pos, len: 0, capacity: Self::INIT_CAPACITY },
+        ];
+    }
+
+    /// Get the absolute position of the inner reader.
+    fn inner_pos(&self) -> u64 {
+        cmp::max(
+            self.part[0].base_pos + self.part[0].len as u64, 
+            self.part[1].base_pos + self.part[1].len as u64)
+    }
+
+    /// Get the absolute position of the stream.
+    pub fn pos(&self) -> u64 {
+        let idx = self.part_idx as usize & 0x1;
+        self.part[idx].base_pos + self.part[idx].len as u64 - (self.end_pos as u64 - self.pos as u64)
+    }
+
+    /// Get the number of bytes buffered but not yet read.
+    pub fn buffered_bytes(&self) -> u64 {
+        self.inner_pos() - self.pos()
+    }
+
+    /// Get the number of rewinable bytes.
+    pub fn rewindable_bytes(&self) -> u64 {
+        self.pos() - cmp::min(self.part[0].base_pos, self.part[1].base_pos)
+    }
+
+    /// Rewinds the stream by the specified number of bytes. 
+    /// 
+    /// Initially, the stream is guaranteed to be rewindable by up-to 1024 bytes. However, rewinds accumulate and the 
+    /// amount the caller can rewind is reduced each call. The rewinable amount can be restored by reading. If at every 
+    /// point in the life of a stream the sum of all read lengths - the sum of all rewind lengths >= 0, rewind will 
+    /// never fail.
+    /// 
+    /// The guaranteed rewinable amount is directly set by the read-ahead capacity. Since `MediaSourceStream` 
+    /// exponentially grows the read-ahead capacity, then the number of rewindable bytes can be much larger than 1024. 
+    /// Use the `rewindable_bytes()` function to determine the amount of rewinable bytes at any time.
+    pub fn rewind(&mut self, rewind_len: usize) -> usize {
+        let cur_idx = self.part_idx as usize & 0x1;
+        let alt_idx = self.part_idx.wrapping_sub(1) as usize & 0x1;
+
+        // Calculate the desired target position to rewind to.
+        let target_pos = self.pos() - rewind_len as u64;
+
+        // The target position is within the current active buffer partition. Rewind the read position boundary.
+        if target_pos >= self.part[cur_idx].base_pos {
+            self.pos -= rewind_len;
         }
-        Ok(&self.buf[self.pos..self.byte_length])
+        // The target position is within the previous active buffer partition.
+        else if target_pos >= self.part[alt_idx].base_pos {
+            // Decrement the active buffer count and get the new active buffer index.
+            self.part_idx = self.part_idx.wrapping_sub(1);
+            // Update the read boundaries.
+            self.pos = (alt_idx * Self::MAX_CAPACITY) + (target_pos - self.part[alt_idx].base_pos) as usize;
+            self.end_pos = self.pos + self.part[alt_idx].len;
+        }
+        // The target position is outside the stream's buffer entirely.
+        else {
+            return 0
+        }
+
+        rewind_len
+    }
+
+    fn fetch_buffer(&mut self) -> io::Result<&[u8]> {
+        // Reached the fill length of the active buffer.
+        if self.pos >= self.end_pos {
+
+            let cur_idx = self.part_idx as usize & 0x1;
+            let alt_idx = self.part_idx.wrapping_add(1) as usize & 0x1;
+
+            // The active buffer partition has a base position less than the previously active buffer partition. That 
+            // means the stream was rewound. Simply increment the active buffer partition.
+            if self.part[cur_idx].base_pos < self.part[alt_idx].base_pos {
+                // Update the read boundaries.
+                self.pos = alt_idx * Self::MAX_CAPACITY;
+                self.end_pos = self.pos + self.part[alt_idx].len;
+
+                // Swap the buffer partitions.
+                self.part_idx = self.part_idx.wrapping_add(1);
+            }
+            // The active buffer partition has a base position greater than the previously active buffer partition. The
+            // active partition is at the front of the stream.
+            else {
+                // The fill length *may* be less than the maximum capacity of the active buffer partition. To maintain 
+                // the invariant that the rewind buffer partition is always at capacity, then the current active buffer 
+                // partition must be filled to capacity before swapping.
+                if self.part[cur_idx].len < self.part[cur_idx].capacity {
+                    let end = self.pos + (self.part[cur_idx].capacity - self.part[cur_idx].len);
+                    let len = self.inner.read(&mut self.buf[self.pos..end])?;
+                    self.part[cur_idx].len += len;
+                    self.end_pos += len;
+                }
+                // The read-ahead buffer has been filled to capacity, and subsequently read fully. Swap the active 
+                // buffer partition with the old rewind buffer partition and read in new data from the inner reader.
+                else {
+                    // The read boundary initial position is the beginning of the partition in the buffer.
+                    self.pos = alt_idx * Self::MAX_CAPACITY;
+
+                    // Grow the buffer partition capacity exponentially to reduce the overhead of buffering on seeking.
+                    self.cur_capacity = cmp::min(self.cur_capacity << 1, Self::MAX_CAPACITY);
+                    self.part[alt_idx].capacity = self.cur_capacity;
+
+                    // Read into the active buffer partition.
+                    let end = self.pos + self.part[alt_idx].capacity;
+                    let len = self.inner.read(&mut self.buf[self.pos..end])?;
+
+                    // Update base position, and the length of the current partition.
+                    self.part[alt_idx].base_pos = self.part[cur_idx].base_pos + self.part[cur_idx].len as u64;
+                    self.part[alt_idx].len = len;
+
+                    // Swap the buffer partitions.
+                    self.part_idx = self.part_idx.wrapping_add(1);
+
+                    // Update the read boundary end position.
+                    self.end_pos = self.pos + len;
+                }
+            }
+
+            /*
+            eprintln!("part_idx={}, cur_capacity={}, pos={}, end_pos={}", 
+                self.part_idx, self.cur_capacity, self.pos, self.end_pos);
+            eprintln!("part[0] = {{ base_pos={}, len={}, capacity={} }}", 
+                self.part[0].base_pos, self.part[0].len, self.part[0].capacity);
+            eprintln!("part[1] = {{ base_pos={}, len={}, capacity={} }}", 
+                self.part[1].base_pos, self.part[1].len, self.part[1].capacity);
+            */
+        }
+
+        Ok(&self.buf[self.pos..self.end_pos])
     }
 
     fn fetch_buffer_or_eof(&mut self) -> io::Result<()> {
@@ -180,45 +341,6 @@ impl MediaSourceStream {
 
 }
 
-impl io::Read for MediaSourceStream { 
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        // If there is no buffered data, and the read is larger than the read-ahead buffer size,
-        // then read directly from the underlying reader.
-        if self.pos == self.byte_length && buf.len() >= self.buf.len() {
-            return self.inner.read(buf);
-        }
-
-        // Fetch the latest read-ahead buffer, and read bytes it.
-        let bytes_read = self.fetch_buffer()?.read(buf)?;
-
-        // Advance the read-ahead buffer.
-        self.pos += bytes_read;
-
-        Ok(bytes_read)
-    }
-}
-
-impl io::Seek for MediaSourceStream {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        // The current position of the underlying reader is ahead of the current position of the MediaSourceStream by
-        // how ever many bytes have not been read from the read-ahead buffer yet. When seeking from the current position
-        // adjust the position delta to offset that difference.
-        let result = match pos {
-            io::SeekFrom::Current(delta_pos) => {
-                let bytes_ahead = (self.byte_length - self.pos) as i64;
-                self.inner.seek(io::SeekFrom::Current(delta_pos - bytes_ahead))
-            }
-            _ => self.inner.seek(pos)
-        };
-
-        // Invalidate the read-ahead buffer.
-        self.pos = 0;
-        self.byte_length = 0;
-
-        result
-    }
-}
-
 impl MediaSource for MediaSourceStream {
     #[inline]
     fn is_seekable(&self) -> bool {
@@ -229,7 +351,43 @@ impl MediaSource for MediaSourceStream {
     fn len(&self) -> Option<u64> {
         self.inner.len()
     }
-} 
+}
+
+impl io::Read for MediaSourceStream { 
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        // Fetch the latest buffer partition, and read bytes from it.
+        let len = self.fetch_buffer()?.read(buf)?;
+
+        // Advance the read position boundary.
+        self.pos += len;
+
+        Ok(len)
+    }
+}
+
+impl io::Seek for MediaSourceStream {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        // The current position of the underlying reader is ahead of the current position of the MediaSourceStream by
+        // how ever many bytes have not been read from the read-ahead buffer yet. When seeking from the current position
+        // adjust the position delta to offset that difference.
+        let pos = match pos {
+            io::SeekFrom::Current(0) => {
+                return Ok(self.pos())
+            },
+            io::SeekFrom::Current(delta_pos) => {
+                let delta = delta_pos - self.buffered_bytes() as i64;
+                self.inner.seek(io::SeekFrom::Current(delta))
+            },
+            _ => {
+                self.inner.seek(pos)
+            }
+        }?;
+        
+        self.invalidate(pos);
+
+        Ok(pos)
+    }
+}
 
 /// A `Bytestream` provides functions to read bytes and interpret them as little- or big-endian
 /// unsigned integers of varying widths.
@@ -331,7 +489,7 @@ impl Bytestream for MediaSourceStream {
         // This function, read_u8, is inlined for performance. To reduce code bloat, place the
         // read-ahead buffer replenishment in a seperate function. Call overhead will be negligible
         // compared to the actual underlying read.
-        if self.pos >= self.byte_length {
+        if self.pos >= self.end_pos {
             self.fetch_buffer_or_eof()?;
         }
 
@@ -349,7 +507,7 @@ impl Bytestream for MediaSourceStream {
 
         // If the buffer has two bytes available, copy directly from it and skip any safety or
         // buffering checks.
-        if self.pos + 2 < self.byte_length {
+        if self.pos + 2 < self.end_pos {
             unsafe { *double_byte.get_unchecked_mut(0) = *self.buf.get_unchecked(self.pos) }
             self.pos += 1;
             unsafe { *double_byte.get_unchecked_mut(1) = *self.buf.get_unchecked(self.pos) }
@@ -359,7 +517,7 @@ impl Bytestream for MediaSourceStream {
         // buffer, checking if it needs to be replenished.
         else {
             for i in 0..2 {
-                if self.pos >= self.byte_length {
+                if self.pos >= self.end_pos {
                     self.fetch_buffer_or_eof()?;
                 }
                 unsafe { *double_byte.get_unchecked_mut(i) = *self.buf.get_unchecked(self.pos) }
@@ -376,7 +534,7 @@ impl Bytestream for MediaSourceStream {
 
         // If the buffer has three bytes available, copy directly from it and skip any safety or
         // buffering checks.
-        if self.pos + 3 < self.byte_length {
+        if self.pos + 3 < self.end_pos {
             unsafe { *triple_byte.get_unchecked_mut(0) = *self.buf.get_unchecked(self.pos) }
             self.pos += 1;
             unsafe { *triple_byte.get_unchecked_mut(1) = *self.buf.get_unchecked(self.pos) }
@@ -388,7 +546,7 @@ impl Bytestream for MediaSourceStream {
         // buffer, checking if it needs to be replenished.
         else {
             for i in 0..3 {
-                if self.pos >= self.byte_length {
+                if self.pos >= self.end_pos {
                     self.fetch_buffer_or_eof()?;
                 }
                 unsafe { *triple_byte.get_unchecked_mut(i) = *self.buf.get_unchecked(self.pos) }
@@ -405,7 +563,7 @@ impl Bytestream for MediaSourceStream {
 
         // If the buffer has four bytes available, copy directly from it and skip any safety or
         // buffering checks.
-        if self.pos + 4 < self.byte_length {
+        if self.pos + 4 < self.end_pos {
             unsafe { *quad_byte.get_unchecked_mut(0) = *self.buf.get_unchecked(self.pos) }
             self.pos += 1;
             unsafe { *quad_byte.get_unchecked_mut(1) = *self.buf.get_unchecked(self.pos) }
@@ -419,7 +577,7 @@ impl Bytestream for MediaSourceStream {
         // buffer, checking if it needs to be replenished.
         else {
             for i in 0..4 {
-                if self.pos >= self.byte_length {
+                if self.pos >= self.end_pos {
                     self.fetch_buffer_or_eof()?;
                 }
                 unsafe { *quad_byte.get_unchecked_mut(i) = *self.buf.get_unchecked(self.pos) }
@@ -476,6 +634,7 @@ impl<'b, B: Bytestream> Bytestream for &'b mut B {
     fn ignore_bytes(&mut self, count: usize) -> io::Result<()> {
         (*self).ignore_bytes(count)
     }
+
 }
 
 /// An `ErrorDetectingStream` is a passive monitoring stream which computes one or more checksums
