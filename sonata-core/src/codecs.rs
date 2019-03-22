b5ef49204f99274586dd4b3310abc4cc044a9900
Philip Deljanov <philip.deljanov@gmail.com>
2019-03-22 16:45:06 -0400

    Update CodecType listing.

--- sonata-core/src/codecs.rs
+++ sonata-core/src/codecs.rs
@@ -31,53 +31,109 @@ use crate::sample::SampleFormat;
 pub struct CodecType(u32);
 
 /// Null decoder, simply discards all data.
-pub const CODEC_TYPE_NULL: CodecType      = CodecType(0x0);
+pub const CODEC_TYPE_NULL: CodecType             = CodecType(0x0);
+
+// Hardware or Operating System provided codecs
+//---------------------------------------------------------
 
 /// Native Hardware Decoder
-pub const CODEC_TYPE_HWDEC: CodecType     = CodecType(0x1);
+pub const CODEC_TYPE_HWDEC: CodecType            = CodecType(0x1);
 
-/// PCM Codecs
-pub const CODEC_TYPE_PCM_S32LE: CodecType = CodecType(0x100);
-pub const CODEC_TYPE_PCM_S32BE: CodecType = CodecType(0x200);
-pub const CODEC_TYPE_PCM_S24LE: CodecType = CodecType(0x300);
-pub const CODEC_TYPE_PCM_S24BE: CodecType = CodecType(0x400);
-pub const CODEC_TYPE_PCM_S16LE: CodecType = CodecType(0x500);
-pub const CODEC_TYPE_PCM_S16BE: CodecType = CodecType(0x600);
-pub const CODEC_TYPE_PCM_S8: CodecType    = CodecType(0x700);
+// PCM codecs
+//-----------
 
-pub const CODEC_TYPE_PCM_U32LE: CodecType = CodecType(0x800);
-pub const CODEC_TYPE_PCM_U32BE: CodecType = CodecType(0x900);
-pub const CODEC_TYPE_PCM_U24LE: CodecType = CodecType(0xa00);
-pub const CODEC_TYPE_PCM_U24BE: CodecType = CodecType(0xb00);
-pub const CODEC_TYPE_PCM_U16LE: CodecType = CodecType(0xc00);
-pub const CODEC_TYPE_PCM_U16BE: CodecType = CodecType(0xd00);
-pub const CODEC_TYPE_PCM_U8: CodecType    = CodecType(0xe00);
+/// PCM signed 32-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_S32LE: CodecType        = CodecType(0x100);
+/// PCM signed 32-bit little-endian planar
+pub const CODEC_TYPE_PCM_S32LE_PLANAR: CodecType = CodecType(0x101);
+/// PCM signed 32-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_S32BE: CodecType        = CodecType(0x102);
+/// PCM signed 32-bit big-endian planar
+pub const CODEC_TYPE_PCM_S32BE_PLANAR: CodecType = CodecType(0x103);
+/// PCM signed 24-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_S24LE: CodecType        = CodecType(0x104);
+/// PCM signed 24-bit little-endian planar
+pub const CODEC_TYPE_PCM_S24LE_PLANAR: CodecType = CodecType(0x105);
+/// PCM signed 24-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_S24BE: CodecType        = CodecType(0x106);
+/// PCM signed 24-bit big-endian planar
+pub const CODEC_TYPE_PCM_S24BE_PLANAR: CodecType = CodecType(0x107);
+/// PCM signed 16-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_S16LE: CodecType        = CodecType(0x108);
+/// PCM signed 16-bit little-endian planar
+pub const CODEC_TYPE_PCM_S16LE_PLANAR: CodecType = CodecType(0x109);
+/// PCM signed 16-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_S16BE: CodecType        = CodecType(0x10a);
+/// PCM signed 16-bit big-endian planar
+pub const CODEC_TYPE_PCM_S16BE_PLANAR: CodecType = CodecType(0x10b);
+/// PCM signed 8-bit interleaved
+pub const CODEC_TYPE_PCM_S8: CodecType           = CodecType(0x10c);
+/// PCM signed 8-bit planar
+pub const CODEC_TYPE_PCM_S8_PLANAR: CodecType    = CodecType(0x10d);
+/// PCM unsigned 32-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_U32LE: CodecType        = CodecType(0x10e);
+/// PCM unsigned 32-bit little-endian planar
+pub const CODEC_TYPE_PCM_U32LE_PLANAR: CodecType = CodecType(0x10f);
+/// PCM unsigned 32-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_U32BE: CodecType        = CodecType(0x110);
+/// PCM unsigned 32-bit big-endian planar
+pub const CODEC_TYPE_PCM_U32BE_PLANAR: CodecType = CodecType(0x111);
+/// PCM unsigned 24-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_U24LE: CodecType        = CodecType(0x112);
+/// PCM unsigned 24-bit little-endian planar
+pub const CODEC_TYPE_PCM_U24LE_PLANAR: CodecType = CodecType(0x113);
+/// PCM unsigned 24-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_U24BE: CodecType        = CodecType(0x114);
+/// PCM unsigned 24-bit big-endian planar
+pub const CODEC_TYPE_PCM_U24BE_PLANAR: CodecType = CodecType(0x115);
+/// PCM unsigned 16-bit little-endian interleaved
+pub const CODEC_TYPE_PCM_U16LE: CodecType        = CodecType(0x116);
+/// PCM unsigned 16-bit little-endian planar
+pub const CODEC_TYPE_PCM_U16LE_PLANAR: CodecType = CodecType(0x117);
+/// PCM unsigned 16-bit big-endian interleaved
+pub const CODEC_TYPE_PCM_U16BE: CodecType        = CodecType(0x118);
+/// PCM unsigned 16-bit big-endian planar
+pub const CODEC_TYPE_PCM_U16BE_PLANAR: CodecType = CodecType(0x119);
+/// PCM unsigned 8-bit interleaved
+pub const CODEC_TYPE_PCM_U8: CodecType           = CodecType(0x11a);
+/// PCM unsigned 8-bit planar
+pub const CODEC_TYPE_PCM_U8_PLANAR: CodecType    = CodecType(0x11b);
+/// PCM 32-bit little-endian floating point interleaved
+pub const CODEC_TYPE_PCM_F32LE: CodecType        = CodecType(0x11c);
+/// PCM 32-bit little-endian floating point planar
+pub const CODEC_TYPE_PCM_F32LE_PLANAR: CodecType = CodecType(0x11d);
+/// PCM 32-bit big-endian floating point interleaved
+pub const CODEC_TYPE_PCM_F32BE: CodecType        = CodecType(0x11e);
+/// PCM 32-bit big-endian floating point planar
+pub const CODEC_TYPE_PCM_F32BE_PLANAR: CodecType = CodecType(0x11f);
+/// PCM 64-bit little-endian floating point interleaved
+pub const CODEC_TYPE_PCM_F64LE: CodecType        = CodecType(0x120);
+/// PCM 64-bit little-endian floating point planar
+pub const CODEC_TYPE_PCM_F64LE_PLANAR: CodecType = CodecType(0x121);
+/// PCM 64-bit big-endian floating point interleaved
+pub const CODEC_TYPE_PCM_F64BE: CodecType        = CodecType(0x122);
+/// PCM 64-bit big-endian floating point planar
+pub const CODEC_TYPE_PCM_F64BE_PLANAR: CodecType = CodecType(0x123);
+/// PCM A-law
+pub const CODEC_TYPE_PCM_ALAW: CodecType         = CodecType(0x124);
+/// PCM Mu-law
+pub const CODEC_TYPE_PCM_MULAW: CodecType        = CodecType(0x125);
 
-pub const CODEC_TYPE_PCM_F32LE: CodecType = CodecType(0xf00);
-pub const CODEC_TYPE_PCM_F32BE: CodecType = CodecType(0x1000);
-pub const CODEC_TYPE_PCM_F64LE: CodecType = CodecType(0x1100);
-pub const CODEC_TYPE_PCM_F64BE: CodecType = CodecType(0x1200);
-
-pub const CODEC_TYPE_PCM_ALAW: CodecType  = CodecType(0x1300);
-pub const CODEC_TYPE_PCM_MULAW: CodecType = CodecType(0x1400);
+// Regular codecs
+//---------------
 
 /// Free Lossless Audio Codec (FLAC)
-pub const CODEC_TYPE_FLAC: CodecType      = CodecType(0x10000);
-
+pub const CODEC_TYPE_FLAC: CodecType             = CodecType(0x1000);
 /// MPEG Layer 1, 2, and 3 (MP1, MP2, MP3)
-pub const CODEC_TYPE_MP3: CodecType       = CodecType(0x20000);
-
+pub const CODEC_TYPE_MP3: CodecType              = CodecType(0x2000);
 /// Advanced Audio Coding (AAC)
-pub const CODEC_TYPE_AAC: CodecType       = CodecType(0x30000);
-
+pub const CODEC_TYPE_AAC: CodecType              = CodecType(0x3000);
 /// Vorbis 
-pub const CODEC_TYPE_VORBIS: CodecType    = CodecType(0x40000);
-
+pub const CODEC_TYPE_VORBIS: CodecType           = CodecType(0x4000);
 /// Opus
-pub const CODEC_TYPE_OPUS: CodecType      = CodecType(0x50000);
-
+pub const CODEC_TYPE_OPUS: CodecType             = CodecType(0x5000);
 /// WavPack
-pub const CODEC_TYPE_WAVPACK: CodecType   = CodecType(0x60000);
+pub const CODEC_TYPE_WAVPACK: CodecType          = CodecType(0x6000);
 
 
 /// Codec parameters stored in a container format's headers and metadata may be passed to a codec using the 
@@ -100,9 +156,6 @@ pub struct CodecParameters {
 
     /// The number of bits per one encoded audio sample.
     pub bits_per_coded_sample: Option<u32>,
-
-    /// The number of audio channels.
-    pub n_channels: Option<u32>,
 
     /// A list of in-order channels.
     pub channels: Option<Channels>,
@@ -129,7 +182,6 @@ impl CodecParameters {
             sample_format: None,
             bits_per_sample: None,
             bits_per_coded_sample: None,
-            n_channels: None,
             channels: None,
             channel_layout: None,
             leading_padding: None,
