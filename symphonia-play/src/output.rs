31911f6ee8403c67ece58ef2868dec89def31a74
Philip Deljanov <philip.deljanov@gmail.com>
2020-08-02 22:14:33 -0400

    play: See last commit; missing output module file.

---
+++ symphonia-play/src/output.rs
@@ -0,0 +1,291 @@
+// Symphonia
+// Copyright (c) 2019 The Project Symphonia Developers.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Platform-dependant Audio Outputs
+
+use std::result;
+
+use symphonia::core::audio::{AudioBufferRef, SignalSpec};
+use symphonia::core::units::Duration;
+
+pub trait AudioOutput {
+    fn write(&mut self, decoded: AudioBufferRef<'_>) -> Result<()>;
+    fn flush(&mut self);
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum AudioOutputError {
+    OpenStreamError,
+    PlayStreamError,
+    StreamClosedError,
+}
+
+pub type Result<T> = result::Result<T, AudioOutputError>;
+
+#[cfg(target_os = "linux")]
+mod pulseaudio {
+    use super::{AudioOutput, AudioOutputError, Result};
+
+    use symphonia;
+    use symphonia::core::audio::*;
+    use symphonia::core::units::Duration;
+
+    use libpulse_binding as pulse;
+    use libpulse_simple_binding as psimple;
+
+    use log::error;
+
+    pub struct PulseAudioOutput {
+        pa: psimple::Simple,
+        sample_buf: RawSampleBuffer<f32>,
+    }
+
+    impl PulseAudioOutput {
+        pub fn try_open(spec: SignalSpec, duration: Duration) -> Result<Box<dyn AudioOutput>> {
+            // An interleaved buffer is required to send data to PulseAudio. Use a SampleBuffer to
+            // move data between Symphonia AudioBuffers and the byte buffers required by PulseAudio.
+            let sample_buf = RawSampleBuffer::<f32>::new(duration, spec);
+
+            // Create a PulseAudio stream specification.
+            let pa_spec = pulse::sample::Spec {
+                format: pulse::sample::SAMPLE_FLOAT32NE,
+                channels: spec.channels.count() as u8,
+                rate: spec.rate,
+            };
+
+            assert!(pa_spec.is_valid());
+
+            // PulseAudio seems to not play very short audio buffers, use thse custom buffer
+            // attributes for very short audio streams.
+            //
+            // let pa_buf_attr = pulse::def::BufferAttr {
+            //     maxlength: std::u32::MAX,
+            //     tlength: 1024,
+            //     prebuf: std::u32::MAX,
+            //     minreq: std::u32::MAX,
+            //     fragsize: std::u32::MAX,
+            // };
+
+            // Create a PulseAudio connection.
+            let pa_result = psimple::Simple::new(
+                None,                               // Use default server
+                "Symphonia Player",                 // Application name
+                pulse::stream::Direction::Playback, // Playback stream
+                None,                               // Default playback device
+                "Music",                            // Description of the stream
+                &pa_spec,                           // Signal specificaiton
+                None,                               // Default channel map
+                None                                // Custom buffering attributes
+            );
+
+            match pa_result {
+                Ok(pa) => {
+                    Ok(Box::new(PulseAudioOutput { pa, sample_buf }))
+                }
+                Err(err) => {
+                    error!("audio output stream open error: {}", err);
+
+                    Err(AudioOutputError::OpenStreamError)
+                }
+            }
+        }
+    }
+
+    impl AudioOutput for PulseAudioOutput {
+        fn write(&mut self, decoded: AudioBufferRef<'_>) -> Result<()> {
+            // Interleave samples from the audio buffer into the sample buffer.
+            self.sample_buf.copy_interleaved_ref(decoded);
+
+            // Write interleaved samples to PulseAudio.
+            match self.pa.write(self.sample_buf.as_bytes()) {
+                Err(err) => {
+                    error!("audio output stream write error: {}", err);
+
+                    Err(AudioOutputError::StreamClosedError)
+                }
+                _ => Ok(())
+            }
+        }
+
+        fn flush(&mut self) {
+            // Flush is best-effort, ignore the returned result.
+            let _ = self.pa.drain();
+        }
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod cpal {
+    use super::{AudioOutput, AudioOutputError, Result};
+
+    use symphonia;
+    use symphonia::core::audio::{AudioBufferRef, SampleBuffer, SignalSpec, WriteSample};
+    use symphonia::core::conv::ConvertibleSample;
+    use symphonia::core::units::Duration;
+
+    use cpal;
+    use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+    use rb::*;
+
+    use log::error;
+
+    pub struct CpalAudioOutput;
+
+    trait AudioOutputSample :
+        cpal::Sample +
+        ConvertibleSample +
+        WriteSample +
+        std::marker::Send +
+        'static {}
+
+    impl AudioOutputSample for f32 { }
+    impl AudioOutputSample for i16 { }
+    impl AudioOutputSample for u16 { }
+
+    impl CpalAudioOutput {
+        pub fn try_open(spec: SignalSpec, duration: Duration) -> Result<Box<dyn AudioOutput>> {
+            // Get default host.
+            let host = cpal::default_host();
+
+            // Get the default audio output device.
+            let device = match host.default_output_device() {
+                Some(device) => device,
+                _ => {
+                    error!("failed to get default audio output device");
+                    return Err(AudioOutputError::OpenStreamError);
+                }
+            };
+
+            let config = match device.default_output_config() {
+                Ok(config) => config,
+                Err(err) => {
+                    error!("failed to get default audio output device config: {}", err);
+                    return Err(AudioOutputError::OpenStreamError);
+                }
+            };
+
+            // Select proper playback routine based on sample format.
+            match config.sample_format() {
+                cpal::SampleFormat::F32 => {
+                    CpalAudioOutputImpl::<f32>::try_open(spec, duration, &device)
+                }
+                cpal::SampleFormat::I16 => {
+                    CpalAudioOutputImpl::<i16>::try_open(spec, duration, &device)
+                }
+                cpal::SampleFormat::U16 => {
+                    CpalAudioOutputImpl::<u16>::try_open(spec, duration, &device)
+                }
+            }
+        }
+    }
+
+    struct CpalAudioOutputImpl<T: AudioOutputSample>
+    where
+        T: AudioOutputSample
+    {
+        ring_buf_producer: rb::Producer<T>,
+        sample_buf: SampleBuffer<T>,
+        stream: cpal::Stream,
+    }
+
+    impl<T: AudioOutputSample> CpalAudioOutputImpl<T>
+    {
+        pub fn try_open(
+            spec: SignalSpec,
+            duration: Duration,
+            device: &cpal::Device
+        ) -> Result<Box<dyn AudioOutput>>
+        {
+            // Output audio stream config.
+            let config = cpal::StreamConfig {
+                channels: spec.channels.count() as cpal::ChannelCount,
+                sample_rate: cpal::SampleRate(spec.rate)
+            };
+
+            // Instantiate a ring buffer capable of buffering 8K (arbitrarily chosen) samples.
+            let ring_buf = SpscRb::new(8 * 1024);
+            let (ring_buf_producer, ring_buf_consumer) = (ring_buf.producer(), ring_buf.consumer());
+
+            let stream_result = device.build_output_stream(
+                &config,
+                move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+                    // Write out as many samples as possible from the ring buffer to the audio
+                    // output.
+                    let written = ring_buf_consumer.read(data).unwrap_or(0);
+                    // Mute any remaining samples.
+                    data[written..].iter_mut().for_each(|s| *s = T::MID);
+                },
+                move |err| {
+                    error!("audio output error: {}", err)
+                },
+            );
+
+            if let Err(err) = stream_result {
+                error!("audio output stream open error: {}", err);
+
+                return Err(AudioOutputError::OpenStreamError);
+            }
+
+            let stream = stream_result.unwrap();
+
+            // Start the output stream.
+            if let Err(err) = stream.play() {
+                error!("audio output stream play error: {}", err);
+
+                return Err(AudioOutputError::PlayStreamError);
+            }
+
+            let sample_buf = SampleBuffer::<T>::new(duration, spec);
+
+            Ok(Box::new(CpalAudioOutputImpl { ring_buf_producer, sample_buf, stream }))
+        }
+    }
+
+    impl<T: AudioOutputSample> AudioOutput for CpalAudioOutputImpl<T>
+    {
+        fn write(&mut self, decoded: AudioBufferRef<'_>) -> Result<()> {
+            // Audio samples must be interleaved for cpal. Interleave the samples in the audio
+            // buffer into the sample buffer.
+            self.sample_buf.copy_interleaved_ref(decoded);
+
+            let mut i = 0;
+
+            // Write out all samples in the sample buffer to the ring buffer.
+            while i < self.sample_buf.len() {
+                let writeable_samples = &self.sample_buf.samples()[i..];
+
+                // Write as many samples as possible to the ring buffer. This blocks until some
+                // samples are written or the consumer has been destroyed (None is returned).
+                if let Some(written) = self.ring_buf_producer.write_blocking(writeable_samples) {
+                    i += written;
+                }
+                else {
+                    // Consumer destroyed, return an error.
+                    return Err(AudioOutputError::StreamClosedError);
+                }
+            }
+
+            Ok(())
+        }
+
+        fn flush(&mut self) {
+            // Flush is best-effort, ignore the returned result.
+            let _ = self.stream.pause();
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn try_open(spec: SignalSpec, duration: Duration) -> Result<Box<dyn AudioOutput>> {
+    pulseaudio::PulseAudioOutput::try_open(spec, duration)
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn try_open(spec: SignalSpec, duration: Duration) -> Result<Box<dyn AudioOutput>> {
+    cpal::CpalAudioOutput::try_open(spec, duration)
+}
