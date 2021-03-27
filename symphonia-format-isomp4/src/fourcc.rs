07d4260c2d06c184f24475c4d8b240d73caa5d88
John-John Tedro <udoprog@tedro.se>
2021-03-27 02:45:07 +0100

    mp4: Support text track handlers and all FourCC values (#23)

--- symphonia-format-isomp4/src/fourcc.rs
+++ symphonia-format-isomp4/src/fourcc.rs
@@ -5,26 +5,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::fmt;
+
 /// Four character codes for typical Ftyps (reference: http://ftyps.com/).
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Clone, Copy)]
+#[repr(transparent)]
 pub struct FourCc {
-    pub val: String,
+    val: [u8; 4],
 }
 
 impl FourCc {
-
-    pub fn from_bytes(val: [u8; 4]) -> Option<Self> {
-        let mut fourcc = FourCc { val: String::new() };
-
-        for &byte in val.iter() {
-            // The characters of a FourCC must be ASCII printable characters.
-            if byte < 0x20 || byte > 0x7e {
-                return None;
-            }
-            fourcc.val.push(char::from(byte));
-        }
-
-        Some(fourcc)
+    /// Construct a new FourCC code from the given byte array.
+    pub fn new(val: [u8; 4]) -> Self {
+        Self { val }
     }
+}
 
+impl fmt::Debug for FourCc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        String::from_utf8_lossy(&self.val).fmt(f)
+    }
 }
