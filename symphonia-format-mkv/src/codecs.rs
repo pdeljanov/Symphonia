b8234f3a96088ba37e674a5058f41d6fbe33743e
darksv <darek969-12@o2.pl>
2021-11-18 18:31:45 +0100

    mkv: add variants of AAC

--- symphonia-format-mkv/src/codecs.rs
+++ symphonia-format-mkv/src/codecs.rs
@@ -13,7 +13,9 @@ pub(crate) fn codec_id_to_type(track: &TrackElement) -> Option<CodecType> {
         "A_FLAC" => Some(codecs::CODEC_TYPE_FLAC),
         "A_OPUS" => Some(codecs::CODEC_TYPE_OPUS),
         "A_VORBIS" => Some(codecs::CODEC_TYPE_VORBIS),
-        "A_AAC" => Some(codecs::CODEC_TYPE_AAC),
+        "A_AAC/MPEG2/MAIN" | "A_AAC/MPEG2/LC" | "A_AAC/MPEG2/LC/SBR" | "A_AAC/MPEG2/SSR" |
+        "A_AAC/MPEG4/MAIN" | "A_AAC/MPEG4/LC" | "A_AAC/MPEG4/LC/SBR" | "A_AAC/MPEG4/SSR" |
+        "A_AAC/MPEG4/LTP" | "A_AAC" => Some(codecs::CODEC_TYPE_AAC),
         "A_PCM/INT/BIG" => match bit_depth? {
             16 => Some(codecs::CODEC_TYPE_PCM_S16BE),
             24 => Some(codecs::CODEC_TYPE_PCM_S24BE),
@@ -28,10 +30,9 @@ pub(crate) fn codec_id_to_type(track: &TrackElement) -> Option<CodecType> {
         }
         "A_PCM/FLOAT/IEEE" => match bit_depth? {
             32 => Some(codecs::CODEC_TYPE_PCM_F32LE),
-            64 => Some(codecs::CODEC_TYPE_PCM_F32BE),
+            64 => Some(codecs::CODEC_TYPE_PCM_F64LE),
             _ => None,
         }
-        // TODO
         _ => None,
     }
 }
