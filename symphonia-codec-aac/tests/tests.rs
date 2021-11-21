a723c15c7ff7181ac4c1c729edff271dcef96d2e
Philip Deljanov <philip.deljanov@gmail.com>
2021-11-20 21:37:04 -0500

    aac: Fix test that broke last commit.

--- symphonia-codec-aac/tests/tests.rs
+++ symphonia-codec-aac/tests/tests.rs
@@ -1,18 +1,19 @@
-use symphonia_core::codecs::{CodecParameters, Decoder, DecoderOptions};
+use symphonia_core::codecs::{CODEC_TYPE_AAC, CodecParameters, Decoder, DecoderOptions};
 use symphonia_core::errors;
 use symphonia_core::formats::{FormatOptions, FormatReader};
+use symphonia_core::io::MediaSourceStream;
+use symphonia_codec_aac::{AacDecoder, AdtsReader};
 
 fn test_decode(data: Vec<u8>) -> symphonia_core::errors::Result<()> {
     let data = std::io::Cursor::new(data);
 
-    let source = symphonia_core::io::MediaSourceStream::new(Box::new(data), Default::default());
+    let source = MediaSourceStream::new(Box::new(data), Default::default());
 
-    let mut reader =
-        symphonia_codec_aac::AdtsReader::try_new(source, &FormatOptions::default())?;
+    let mut reader = AdtsReader::try_new(source, &FormatOptions::default())?;
 
-    let mut decoder = symphonia_codec_aac::AacDecoder::try_new(
-        &CodecParameters::default(),
-        &DecoderOptions::default(),
+    let mut decoder = AacDecoder::try_new(
+        CodecParameters::new().for_codec(CODEC_TYPE_AAC),
+        &DecoderOptions::default()
     )?;
 
     loop {
