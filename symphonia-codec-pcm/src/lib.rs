1bd4f35835499b809fd2a6096b127352b6d310ee
Philip Deljanov <philip.deljanov@gmail.com>
2024-01-28 21:40:30 -0500

    pcm: Clarify SUN license for mu-law and a-law decoding.
    Fixes #255.

--- symphonia-codec-pcm/src/lib.rs
+++ symphonia-codec-pcm/src/lib.rs
@@ -173,7 +173,33 @@ macro_rules! read_pcm_transfer_func {
 }
 
 // alaw_to_linear and mulaw_to_linear are adaptations of alaw2linear and ulaw2linear from g711.c by
-// SUN Microsystems (unrestricted use license).
+// SUN Microsystems (unrestricted use license), license text for these parts follow below.
+//
+// ---
+//
+// This source code is a product of Sun Microsystems, Inc. and is provided for
+// unrestricted use.  Users may copy or modify this source code without
+// charge.
+//
+// SUN SOURCE CODE IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE
+// WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.
+//
+// Sun source code is provided with no support and without any obligation on the
+// part of Sun Microsystems, Inc. to assist in its use, correction,
+// modification or enhancement.
+//
+// SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE
+// INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY THIS SOFTWARE
+// OR ANY PART THEREOF.
+//
+// In no event will Sun Microsystems, Inc. be liable for any lost revenue
+// or profits or other special, indirect and consequential damages, even if
+// Sun has been advised of the possibility of such damages.
+//
+// Sun Microsystems, Inc.
+// 2550 Garcia Avenue
+// Mountain View, California  94043
 const XLAW_QUANT_MASK: u8 = 0x0f;
 const XLAW_SEG_MASK: u8 = 0x70;
 const XLAW_SEG_SHIFT: u32 = 4;
