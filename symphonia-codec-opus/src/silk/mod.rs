901f273238ac32d29593e0a0fa5de90b77a2f173
jiztastamablastamarang <jiztastamablastamarang@gmail.com>
2024-09-24 07:49:10 +0300

    Refactor codebook and icdf

--- symphonia-codec-opus/src/silk/mod.rs
+++ symphonia-codec-opus/src/silk/mod.rs
@@ -6,8 +6,7 @@ mod table;
 mod codebook;
 mod error;
 mod decoder;
-mod icdf;
 
-pub (crate) use decoder::Decoder;
+pub(crate) use decoder::Decoder;
 pub use error::Error;
 pub use frame::{Frame, FrameType, QuantizationOffsetType};
