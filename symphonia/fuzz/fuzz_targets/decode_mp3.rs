93e36c6cd8e9fb635303f09d75541682b7e6d431
Mrmaxmeier <Mrmaxmeier@gmail.com>
2026-01-06 23:26:31 +0100

    update fuzzers to new APIs

--- symphonia/fuzz/fuzz_targets/decode_mp3.rs
+++ symphonia/fuzz/fuzz_targets/decode_mp3.rs
@@ -1,11 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use symphonia::core::codecs::{CODEC_ID_MP3, CodecParameters, Decoder};
+use symphonia::core::codecs::audio::well_known::CODEC_ID_MP3;
+use symphonia::core::codecs::audio::{AudioCodecParameters, AudioDecoder};
 use symphonia::core::formats::Packet;
 use symphonia::default::codecs::MpaDecoder;
 
 fuzz_target!(|data: Vec<u8>| {
-    let mut codec_params = CodecParameters::new();
+    let mut codec_params = AudioCodecParameters::new();
     codec_params.for_codec(CODEC_ID_MP3);
 
     let mut decoder = MpaDecoder::try_new(&codec_params, &Default::default()).unwrap();
