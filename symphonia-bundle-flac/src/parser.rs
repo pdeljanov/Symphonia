042cd7ccace94cfd6cce0048c44dfdb9b7e76116
tsar-boomba <itg.2048@gmail.com>
2026-02-24 10:35:34 -0500

    flac no_std; tansition complete :sparkles:

--- symphonia-bundle-flac/src/parser.rs
+++ symphonia-bundle-flac/src/parser.rs
@@ -5,6 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use alloc::boxed::Box;
+use alloc::vec::Vec;
 use symphonia_common::xiph::audio::flac::StreamInfo;
 use symphonia_core::checksum::Crc16Ansi;
 use symphonia_core::errors::{Error, Result};
